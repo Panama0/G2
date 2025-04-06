75f95791499726fb24c18144753a00c23ef2f59f
Panama0 <tjlbates@gmail.com>
2025-04-06 11:42:43 +1000

    Added Parser class to improve flexibility of parsing. Currently it is bound tightly to map saves, in the future it is intended to be generic. Data validation also to be added.

--- src/scene base/GameMap.cpp
+++ src/scene base/GameMap.cpp
@@ -1,6 +1,7 @@
 #include "scene base/GameMap.hpp"
 
 #include "engine/Serialisation.hpp"
+#include "engine/Parser.hpp"
 
 #include <fstream>
 #include <iostream>
@@ -70,16 +71,16 @@ std::optional<GameMap::MapTile> GameMap::getTileAt(const sf::Vector2f& pos)
 
 [[nodiscard]] bool GameMap::save(const std::filesystem::path& path)
 {
-    Serialisation outS;
+    Serialisation save;
 
-    if(!outS.begin(path, std::ios::out))
+    if(!save.begin(path, std::ios::out))
     {
         std::cerr << "Failed to save game!\n";
         return false;
     }
     
 
-    outS.beginSection("MetaData");
+    save.beginSection("MetaData");
     
     auto now = std::chrono::system_clock::now();
     auto timeNow = std::chrono::system_clock::to_time_t(now);
@@ -88,51 +89,48 @@ std::optional<GameMap::MapTile> GameMap::getTileAt(const sf::Vector2f& pos)
     ss << std::put_time(std::localtime(&timeNow), "%Y-%m-%d %X");
     std::string identString = ss.str();
     
-    outS.writeLine(identString);
+    save.writeLineBuffer(identString);
     
-    outS.writeLineBuffer("GridSize:");
-    outS.writeLineBuffer(std::to_string(m_gridSize.x), ",");
-    outS.writeLineBuffer(std::to_string(m_gridSize.y));
-    outS.endLine();
+    save.writeLineBuffer(std::to_string(m_gridSize.x), ",");
+    save.writeLineBuffer(std::to_string(m_gridSize.y));
     
-    outS.writeLineBuffer("WorldSize:");
-    outS.writeLineBuffer(std::to_string(m_worldSize.x));
-    outS.writeLineBuffer(std::to_string(m_worldSize.y));
-    outS.endLine();
+    save.writeLineBuffer(std::to_string(m_worldSize.x));
+    save.writeLineBuffer(std::to_string(m_worldSize.y), ",");
+    save.endLine();
     
-    outS.endSection();
+    save.endSection();
     
     if(m_tiles.size() > 0)
     {
-        outS.beginSection("TileData");
+        save.beginSection("TileData");
 
         for(const auto& tile : m_tiles)
         {
-            outS.beginSection("Tile");
+            save.beginSection("Tile");
             
-            outS.writeLineBuffer(tile.textureName);
-            outS.writeLineBuffer(std::to_string(tile.pos.x), ",");
-            outS.writeLineBuffer(std::to_string(tile.pos.y));
-            outS.writeLineBuffer(std::to_string(tile.rotation.asRadians()));
-            outS.writeLineBuffer(std::to_string(tile.id));
-            outS.endLine();
+            save.writeLineBuffer(tile.textureName);
+            save.writeLineBuffer(std::to_string(tile.pos.x), ",");
+            save.writeLineBuffer(std::to_string(tile.pos.y));
+            save.writeLineBuffer(std::to_string(tile.rotation.asRadians()));
+            save.writeLineBuffer(std::to_string(tile.id));
+            save.endLine();
             
             if (!tile.effects.empty())
             {
-                outS.beginSection("Effects");
+                save.beginSection("Effects");
                 for(const auto& effect : tile.effects)
                 {
-                    outS.writeLineBuffer(effect.textureName);
-                    outS.writeLineBuffer(TileEffect::toString(effect.effect));
-                    outS.endLine();
+                    save.writeLineBuffer(effect.textureName);
+                    save.writeLineBuffer(TileEffect::toString(effect.effect));
+                    save.endLine();
                 }
-                outS.endSection();
+                save.endSection();
             }
             
-            outS.endSection();
+            save.endSection();
         }
 
-        outS.endSection();
+        save.endSection();
     }
     
     return true;
@@ -140,111 +138,23 @@ std::optional<GameMap::MapTile> GameMap::getTileAt(const sf::Vector2f& pos)
 
 [[nodiscard]] bool GameMap::load(const std::filesystem::path& path)
 {
-    std::ifstream in {path};
     
-    if(!in)
+    Parser p;
+    if(!p.begin(path))
     {
-        std::cerr << "Could not load game!\n";
-        return false;
+        std::cerr << "Could not open save!\n";
     }
-    
-    clear();
-    
-    std::string token;
-    std::string buffer;
-    
 
-    // in >> token;
+    clear();
+
+    auto data = p.parseSave();
     
-    // if(token != m_identifier)
-    // {
-    //     std::cerr << "Not a valid save file\n";
-    //     return false;
-    // }
-    
-    // while(in >> token)
-    // {
-    //     if(token == "GridSize:")
-    //     {
-    //         std::string gridSize;
-    //         in >> gridSize;
-            
-    //         m_gridSize = stovec<uint32_t>(gridSize);
-    //     }
-    //     if(token == "WorldSize:")
-    //     {
-    //         std::string worldSize;
-    //         in >> worldSize;
-            
-    //         m_worldSize = stovec<float>(worldSize);
-    //     }
-    //     if(token == "TileData:")
-    //     {
-    //         std::string texName;
-    //         std::string posStr;
-    //         std::string rotationStr;
-    //         std::string idStr;
-            
-            
-    //         while(token != "EndTileData" && in)
-    //         {
-    //             in >> token;
-                
-    //             if(token != "Tile")
-    //             {
-    //                 std::cerr << "Expected tile!\n";
-    //             }
-                
-    //             in >> texName >> posStr >> rotationStr >> idStr;
-                
-    //             sf::Vector2f pos {stovec<float>(posStr)};
-    //             sf::Angle angle {sf::radians(std::stof(rotationStr))};
-    //             //uint32_t id {static_cast<uint32_t>(std::stoi(idStr))};
-                
-    //             MapTile  tile {pos, angle, texName};
-    //             auto tileId = tile.id;
-                
-    //             m_tiles.emplace_back(tile);
-                
-    //             in >> token;
-    //             if(token == "Effects:")
-    //             {
-    //                 std::string effectStr;
-    //                 while(effectStr != "EndTile" && in)
-    //                 {
-    //                     in >> effectStr;
-                        
-    //                     placeEffect(tileId, TileEffect::fromString(effectStr));
-    //                 }
-    //             }
-    //             else
-    //             {
-    //                 std::cerr << "Effects block expected!\n";
-    //             }
-    //             //in >> token;
-    //         }
-    //     }
-    // }
+    m_worldSize = data.worldSize;
+    m_gridSize = data.gridSize;
+    m_tiles = data.tiles;
+
     
     return true;
-}
-
-template <typename T>
-sf::Vector2<T> GameMap::stovec(std::string_view string)
-{
-    auto midPos {string.find(',')};
-    
-    if(midPos == std::string::npos)
-    {
-        std::cerr << "String to vector failed!, Invalid string passed to func.\n";
-        return sf::Vector2<T> {};
-    }
-    
-    std::string_view x {string.substr(0, midPos)};
-    std::string_view y {string.substr(midPos + 1)};
-    
-    return sf::Vector2<T> {static_cast<T>(std::stof(x.data())),
-                           static_cast<T>(std::stof(y.data()))};
 }
 
 void GameMap::clear()
