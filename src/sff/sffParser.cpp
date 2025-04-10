67cf61d8b53d506b7d8003bd04557d8e188309c5
Panama0 <tjlbates@gmail.com>
2025-04-10 21:12:44 +1000

    Began parser for sff.

---
+++ src/sff/sffParser.cpp
@@ -0,0 +1,97 @@
+#include "sff/sffParser.hpp"
+#include "sff/sffNode.hpp"
+
+#include <string>
+#include <memory>
+#include <filesystem>
+#include <iostream>
+#include <optional>
+
+namespace sff{
+
+bool Parser::open(const std::filesystem::path& path)
+{
+    m_file.open(path);
+    
+    if(path.extension() != ".sff")
+    {
+        std::cerr << "Could not open file: invalid file extension. Expect .sff";
+        return false;
+    }
+    
+    return alive();
+}
+
+std::unique_ptr<Node> Parser::parse(const std::string& rootTag)
+{
+    m_root = std::make_unique<Node>(rootTag);
+    m_currentNode = m_root.get();
+
+    while(alive())
+    {
+        // check for a new node - Name {
+        // check for data - Name =
+        consumeToken(4);
+        
+        std::string test;
+        m_file >> test;
+        
+    }
+    
+    return std::move(m_root);
+}
+
+std::optional<std::string> Parser::peekToken(uint32_t ahead)
+{
+    const auto& posBefore = m_file.tellg();
+    const auto& stateBefore = m_file.rdstate();
+    
+    std::optional<std::string> token;
+    for(uint32_t i {}; i < ahead; i++)
+    {
+        m_file >> token->data();
+    }
+    if(!m_file)
+    {
+        m_file.clear();
+    }
+    // put back everything
+    m_file.seekg(posBefore);
+    m_file.setstate(stateBefore);
+
+    return token;
+}
+
+std::optional<char> Parser::peek(uint32_t ahead)
+{
+    const auto& posBefore = m_file.tellg();
+    const auto& stateBefore = m_file.rdstate();
+    
+    int result {};
+    for(uint32_t i {}; i < ahead; i++)
+    {
+        result = m_file.get();
+    }
+
+    if(result == std::istream::traits_type::eof())
+    {
+        return std::nullopt;
+        m_file.clear();
+    }
+
+    // put back everything
+    m_file.seekg(posBefore);
+    m_file.setstate(stateBefore);
+    return result;
+}
+void Parser::consumeToken(uint32_t ahead)
+{
+    
+}
+
+void Parser::consume(uint32_t ahead)
+{
+    m_file.ignore(ahead);
+}
+
+}
--- src/sff/sffParser.hpp
+++ src/sff/sffParser.hpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <filesystem>
 #include <memory>
+#include <optional>
 
 namespace sff
 {
@@ -13,15 +14,20 @@ class Parser
 public:
     bool open(const std::filesystem::path& path);
     
-    Node* parse();
+    std::unique_ptr<Node> parse(const std::string& rootTag);
     
     bool alive() { return m_file.good(); }
     
     void close();
 
 private:
+    std::optional<std::string> peekToken(uint32_t ahead = 1);
+    std::optional<char> peek(uint32_t ahead = 1);
+    void consumeToken(uint32_t ahead = 1);
+    void consume(uint32_t ahead = 1);
+
     std::unique_ptr<Node> m_root;
-    Node* currentNode;
+    Node* m_currentNode;
     std::fstream m_file;
 };
 }
